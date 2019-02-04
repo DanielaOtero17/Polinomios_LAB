415e1b044e5a99151a2a4a117863fe51d0365e42
DanielaOtero17 <jessot@hotmail.com>
2019-02-03 19:00:43 -0500

    Principal class are created.

---
+++ Laboratorio_Polimonios/src/Interfaz/Principal.java
@@ -0,0 +1,25 @@
+package Interfaz;
+
+import javax.swing.*;
+
+public class Principal extends JFrame {
+	
+	
+	
+	public Principal(){
+		
+		setSize(600,600);
+		setResizable(false);
+		setTitle("LABORATORIO ******* AED ****** POLINOMIOS ****** BIENVENIDO");
+		
+		
+	}
+	
+	public static void main(String []args){
+		
+		Principal main = new Principal();
+		main.setVisible(true);
+		
+	}
+
+}
